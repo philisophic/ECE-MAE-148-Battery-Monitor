3f601fd2f3b41dee7dcc989df9690540f32b557f
Pratik Yadav <45407020+pyadav1992@users.noreply.github.com>
2021-02-26 23:13:38 -0800

    Update teensy-test.ino

--- teensy-test/teensy-test.ino
+++ teensy-test/teensy-test.ino
@@ -1,19 +1,20 @@
 #define TEST_MODE               1     // If test mode is enable then pin-3 simulates the jetson nano wake up signal
 #if TEST_MODE
-  #define WAKE_UP_SIGNAL_PIN    3
+  #define WAKE_UP_SIGNAL_PIN    6
   #define TEST_DELAY            2*1000 // 15 seconds
 #endif        
-#define WAKE_UP_INTERRUPT_PIN 0     // Wake up interrupt pin 
-#define NO_CELLS              3     // Number of cells in the LIPO
-#define ONE_CELL_VOLTAGE      4     // One cell voltage in LIPO 
-#define ADC_REF_VOLTAGE       3.3   // ADC reference voltage
-#define ADC_RESOLUTION        65535 // 16 bit resolution
-#define NUM_ADC_SAMPLE        300   // Number of ADC samples to average
+#define WAKE_UP_INTERRUPT_PIN   2     // Wake up interrupt pin 
+#define NO_CELLS                3     // Number of cells in the LIPO
+#define ONE_CELL_VOLTAGE        4.0   // One cell voltage in LIPO 
+#define ADC_REF_VOLTAGE         3.3   // ADC reference voltage
+#define ADC_RESOLUTION          65535 // 16 bit resolution
+#define NUM_ADC_SAMPLE          300   // Number of ADC samples to average
 
-bool wake_up                    = true;
-uint8_t adc_teensy[NO_CELLS]    = {A0, A3, A6};                                                           // ADC channels to read voltages
-uint8_t cell_voltage[NO_CELLS]  = {(ONE_CELL_VOLTAGE/3), (2*ONE_CELL_VOLTAGE/3), (3*ONE_CELL_VOLTAGE/3)}; // Cells are in series on the LIPO
-double  accumulate[NO_CELLS]    = {0, 0, 0};
+bool    wake_up                 = true;
+int     adc_teensy[NO_CELLS]    = {A0, A3, A6};                                                           // ADC channels to read voltages
+float   cell_voltage_ratios[NO_CELLS]  = {(ONE_CELL_VOLTAGE/3), (2*ONE_CELL_VOLTAGE/3), (3*ONE_CELL_VOLTAGE/3)}; // Cells are in series on the LIPO
+// float   accumulate[NO_CELLS]    = {0, 0, 0};
+float   accumulate = 0;
 float   lipo_voltage[NO_CELLS]  = {0, 0, 0};
 char    jetson_log[6]           = ""; 
 // Setup the Teensy  
@@ -31,8 +32,10 @@ void setup()
 // Main processing loop
 void loop() 
 {
+  Serial.println("Hello \r\n");  
+#if 1
   // Disable global interrupts
-  noInterrupts();
+  // noInterrupts();
     
   // Read ADC if Jetson Nano has requested
   if(wake_up)
@@ -43,16 +46,28 @@ void loop()
     // j = 0 Read one cell voltage       - 4V
     // j = 1 Read two cells voltage      - 8V
     // j = 2 Read the three cell voltage - 12V
-    for (uint8_t j=0; j<NO_CELLS; j++)
+    for (int j=0; j<NO_CELLS; j++)
     {
-      accumulate[j]   = 0;
-      lipo_voltage[j] = 0; 
-	    for (uint8_t i = 0; i < NUM_ADC_SAMPLE; i++) 
-	    {
-	      accumulate[j] += analogRead(adc_teensy[j]) * ADC_REF_VOLTAGE / ADC_RESOLUTION * cell_voltage[j];
+      Serial.print("Cell voltage ratios: ");
+      Serial.print(cell_voltage_ratios[j]);
+      Serial.print(" \n"); 
+      accumulate   = 0.0;
+      lipo_voltage[j] = 0.0;
+            
+	    for (int i = 0; i < NUM_ADC_SAMPLE; i++) 
+	    {      
+        // accumulate = accumulate + (analogRead(A0) * (ADC_REF_VOLTAGE / ADC_RESOLUTION) * cell_voltage[j]);
+        accumulate += analogRead(adc_teensy[j]) * ADC_REF_VOLTAGE / ADC_RESOLUTION * cell_voltage_ratios[j];
 		    delay(1);
 	    }
-      lipo_voltage[j] = accumulate[j]/NUM_ADC_SAMPLE;
+      lipo_voltage[j] = accumulate/NUM_ADC_SAMPLE;
+      Serial.print("j: ");
+      Serial.print(j);
+      Serial.print(" \n");      
+      Serial.print("accumulate: ");
+      Serial.print(accumulate);
+      Serial.print(" \n");  
+          
     }
     // Generate string of voltages for Jetson nano 
     sprintf(jetson_log, "%.2f,%.2f,%.2f\n", lipo_voltage[0], lipo_voltage[1], lipo_voltage[2]);
@@ -63,7 +78,7 @@ void loop()
   }
   
   // Enable global interrupts
-  interrupts();
+  // interrupts();
 
 #if TEST_MODE
   digitalWrite(WAKE_UP_SIGNAL_PIN, LOW); // Set the low
@@ -71,12 +86,17 @@ void loop()
   digitalWrite(WAKE_UP_SIGNAL_PIN, HIGH); // Set the high
   delay(TEST_DELAY);                        
 #endif 
+#endif
 }
 
 // wake up ISR 
 void wake_up_isr (void)
 {
   wake_up = true;
+  // lipo_voltage[0] = 0.0;
+  // lipo_voltage[1] = 0.0;
+  // lipo_voltage[2] = 0.0; 
+  analogReadResolution(16);
 #if TEST_MODE
   Serial.println("Wake up interrupt \r\n");                       
 #endif   
